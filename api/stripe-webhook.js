576f0a2f1378472c3f39b47b19ccfdfbbb640dea
uigalaxygalaxy <158004765+uigalaxygalaxy@users.noreply.github.com>
2025-02-03 17:40:29 -0500

    Update stripe-webhook.js

--- api/stripe-webhook.js
+++ api/stripe-webhook.js
@@ -56,7 +56,7 @@ export default async function handler(req, res) {
 
             You have purchased our goods for ${session.amount_total / 100} ${currency}! 
 
-            Your goods will be shipped to: ${session.shipping.address.line1} ${session.shipping.address.line2}, ${session.shipping.address.postal_code}  ${session.shipping.address.state} ${session.shipping.address.country}!
+            Your goods will be shipped to: ${session.shipping.address}!
 
 
             We will give you your shipping number once we get to your order!
